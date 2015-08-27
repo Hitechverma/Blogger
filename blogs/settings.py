e1fbfc4c15e4403f2ce36b35d1f4aee67d83cd43
hitechverma <hitechlov22@gmail.com>
2015-08-27 12:44:18 +0530

    new

--- blogs/settings.py
+++ blogs/settings.py
@@ -57,7 +57,10 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 ROOT_URLCONF = 'blogs.urls'
--- myblog/views.py
+++ myblog/views.py
@@ -13,3 +13,19 @@ class PostList(APIView):
         posts = userpost.objects.all()
         serializer = PostSerializers(posts,many=True)
         return Response(serializer.data)
+
+    def post(self,request, format=None):
+        print "what the hell  ",request.data
+        try:
+            serializer = PostSerializers(data=request.data,many=True)
+            print "Did you say somth ", serializer
+            if serializer.isValid():
+                serializer.save()
+                pos = userpost.objects.get(username=request.data['username'])
+                print pos
+                return Response(serializer.data)
+            return Response(serializer.errors)
+        except :
+            print "Except"
+            return Response("Not working")
+
