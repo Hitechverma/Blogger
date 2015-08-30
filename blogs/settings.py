f8ead65517f908814dc1c15e7855e22a7be2643c
hitechverma <hitechlov22@gmail.com>
2015-08-31 03:54:08 +0530

    All CORS fixed

--- blogs/settings.py
+++ blogs/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'myblog'
+    'myblog',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -64,6 +66,17 @@ REST_FRAMEWORK = {
 }
 
 ROOT_URLCONF = 'blogs.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_HEADERS = (
+        'x-requested-with',
+        'content-type',
+        'accept',
+        'origin',
+        'authorization',
+        'x-csrftoken'
+    )
 
 TEMPLATES = [
     {
